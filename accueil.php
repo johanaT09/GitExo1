761301cdfc896e7a17746e904dee6a6de35ad4b2
Johana TERRIER <jterr@lmdsio.com>
2024-09-18 14:39:20 +0200

    Test

---
+++ accueil.php
@@ -0,0 +1,14 @@
+Bonjour <?php echo $_POST["prenom"]." ".$_POST['nom']; ?>
+<?php
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+if ($_POST["login"]=="admin" && $_POST["mdp"]=="azerty") {
+    $extra = 'profil.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}else{
+    $extra = 'index.html';
+    header("Location: http://$host$uri/$extra");
+    exit;
+}
+?>
